715160a5108f2e310e70a5f2fb3b460b82f0d8ed
Pei <jzha471@aucklanduni.ac.nz>
2017-08-09 10:38:56 +1200

    42. Creating components ( ng g c A/B --spec false , create component B under folder A without spec )

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+
+@Component(
+  {
+    selector : 'app-recipe-detail' ,
+    templateUrl : './recipe-detail.component.html' ,
+    styles : []
+  }
+)
+
+
+export class RecipeDetailComponent
+{
+
+}
