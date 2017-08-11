9fca4013df16d3973ae4a78797e219117fe6cc4d
Pei <jzha471@aucklanduni.ac.nz>
2017-08-11 20:24:26 +1200

    47. Adding contents to recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 
 import {Component} from '@angular/core';
+import {Recipe} from '../recipe.model';
 @Component({
   selector : 'app-recipe-list',
   templateUrl : './recipe-list.component.html',
-  styles :[]
+  styleUrls :['./recipe-list.component.css']
 })
 
 export class RecipeListComponent{
+  recipes:Recipe[] =[
+    new Recipe('A test recipe', 'this is simple a test' , 'https://www.bbcgoodfood.com/sites/default/files/styles/carousel_medium/public/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg?itok=naGPMoRQ')
+  ] ;
 
 }
