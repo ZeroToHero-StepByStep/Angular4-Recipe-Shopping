bca1555cff5cde33bb67d5a3367f944350aaaa52
Pei <jzha471@aucklanduni.ac.nz>
2017-10-28 14:54:30 +1300

    207. Allowing the User to Clear(Cancel) the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -54,4 +54,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false ;
+  }
+
 }
