2076708e5eef1f047034cb389c6fb6832be85216
Jack <jzha471@aucklanduni.ac.nz>
2017-12-06 18:19:18 +1300

    250.Create a Signup Page and Route ( finish rebase )

---
+++ src/app/auth/signin/signin.component.ts
@@ -0,0 +1,16 @@
+///<reference path="../../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
